d0378b8c21c88948608e48bd1b95a954d2b5de04
dpinezich <david.pinezich@gmail.com>
2024-06-07 09:17:22 +0200

    updated

---
+++ typescript/02_basics/listings/09_downleveling.ts
@@ -0,0 +1,11 @@
+/*
+ tsc 09_downleveling.ts ==> ES3
+ tsc 09_downleveling.ts --target es2015
+ alternative: tsc --project tsconfig.json
+*/
+
+const greet_8 = (person: string, date: Date) => {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet_8("David", new Date());
---
+++ vue/05_vue_components/event-listener/src/components/BlogPost.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: ['title'],
+  emits: ['enlarge-text', 'reduce-text']
+}
+</script>
+
+<template>
+  <div class="blog-post">
+	  <h4>{{ title }}</h4>
+	  <button @click="$emit('enlarge-text')">Enlarge text</button><br>
+    <button @click="$emit('reduce-text')">Reduce text</button>
+  </div>
+</template>
