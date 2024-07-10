27cd2e850892be0c97be6a9b231b4c7b4b785c90
dpinezich <david.pinezich@gmail.com>
2024-07-10 13:13:54 +0200

    cart example

---
+++ vue/41_pinia_cart_example/src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+import Cart from './components/Cart.vue';
+import NavBar from './components/NavBar.vue';
+import { useProductStore } from './stores/ProductStore';
+import { useCartStore } from './stores/CartStore';
+
+const showCart = ref(false);
+
+const productStore = useProductStore();
+const cartStore = useCartStore();
+
+</script>
+
+<template>
+  <div class="relative max-w-6xl mx-auto">
+    <NavBar :show-cart="showCart" @toggleCart="showCart = !showCart"/>
+    <Cart v-if="showCart"/>
+    <main class="flex flex-1">
+      <Card v-for="product in productStore.products" :key="product.name" :product="product" @add-to-cart="cartStore.addItem"/>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ vue/41_pinia_cart_example/src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="absolute top-20 right-0 w-1/3 p-4 border rounded-lg bg-white border-gray-300">
+          <h1 class="text-xl font-bold">My Cart</h1>
+          <ul>
+            <li v-for="[name, items] in Object.entries(cartStore.groupedItems)" :key="name">
+              <div class="flex items-center justify-between py-2">
+                <span>{{name}}</span>
+                <div class="flex items-center">
+                  <button @click="cartStore.removeItem(items[0])" class="hover:bg-gray-200 rounded-full p-2">
+                  -
+                </button>
+                <span class="mx-2">{{ items.length }}</span>
+                <button @click="cartStore.addItem(items[0])" class="hover:bg-gray-200 rounded-full p-2">
+                  +
+                </button>
+                </div>
+                <span>${{items.map(i => i.price).reduce((totalItemPrice, price) => totalItemPrice + price, 0)}}</span>
+              </div>
+            </li>
+          </ul>
+          <hr class="my-2">
+          <div class="flex items-center justify-between">
+            <span class="font-bold">Total</span>
+            <span class="font-bold">${{ cartStore.items.map(p => p.price).reduce((acc,curr) => acc + curr, 0) }}</span>
+          </div>
+        </div>  
+</template>
+
+<script setup lang="ts">
+import { useCartStore } from '../stores/CartStore';
+
+const cartStore = useCartStore();
+</script>
